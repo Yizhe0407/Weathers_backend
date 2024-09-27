5a969ca6c2f5772c3303ceb368270951c546fbf3
yizhe <liaoyizhe75@gmail.com>
2024-09-27 09:24:07 +0800

    Refactor weatherRoutes.js to add createUser endpoint

--- routes/weatherRoutes.js
+++ routes/weatherRoutes.js
@@ -1,10 +1,11 @@
 // routes/weatherRoutes.js
 import express from 'express';
-import { weather, add } from '../controllers/weatherController.js';
+import { weather,createUser ,add } from '../controllers/weatherController.js';
 
 const router = express.Router();
 
 router.post('/weather', weather);
+router.post('/createUser', createUser);
 router.post('/add', add);
 
 export default router;
